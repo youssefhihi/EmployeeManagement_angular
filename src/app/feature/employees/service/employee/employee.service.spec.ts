fc15c2182361a260d6d29cef9c5c4a546e3c81cb
youssefhihi <youssefhihi18@gmail.com>
2025-01-03 16:48:59 +0100

    update EMA-19

--- src/app/feature/employees/service/employee/employee.service.spec.ts
+++ src/app/feature/employees/service/employee/employee.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { EmployeeService } from './employee.service';
+import { Employee } from '../../../types/employee/employee';
 
 describe('EmployeeService', () => {
   let service: EmployeeService;
@@ -12,5 +13,12 @@ describe('EmployeeService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+
+  it('should be create a new employee', () => {
+    service.createEmployee({} as Employee).subscribe();
+    expect(service).toBeTruthy();
+
   });
 });
